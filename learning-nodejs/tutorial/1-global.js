98ad04f4340cf0d57a341ec797d967388f3a0321
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-12-26 21:12:48 +0545

    Simple node program

---
+++ learning-nodejs/tutorial/1-global.js
@@ -0,0 +1,7 @@
+console.log(__dirname); // shows the current directory
+console.log(__filename); // shows the current filename
+
+// prints Hello! in the interval of 1000(idk microsecs or mili)
+setInterval(() => {
+    console.log("Hello!");
+}, 1000)
