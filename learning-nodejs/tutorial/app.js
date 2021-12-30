425b09eafbd734791a31b3276cbe5f2900d597ef
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-12-30 11:14:09 +0545

    Just require('destination') executes the efuntions

---
+++ learning-nodejs/tutorial/app.js
@@ -0,0 +1,7 @@
+const names = require("./2-names");
+const sayHi = require('./2-utils');
+const data = require('./2-alternative-syntax');
+require('./3-mind-grenade')
+// sayHi('susan');
+// sayHi((names.john));
+// sayHi(names.peter);
