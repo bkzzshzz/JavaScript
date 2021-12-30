37b79de0b7b696c60383aa09b1a68014d47d3018
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-12-30 11:26:39 +0545

    Minor update

--- learning-nodejs/tutorial/2-modules.js
+++ learning-nodejs/tutorial/2-modules.js
@@ -1,11 +1,7 @@
-// Modules
-const names = require('./2-names');
+const names = require("./2-names");
 const sayHi = require('./2-utils');
-const data = require('./2-alternative-syntax')
-console.log(names);
-console.log(data);
-
-
-sayHi('Senorita');
-sayHi(names.john);
-sayHi(names.peter)
+const data = require('./2-alternative-syntax');
+require('./3-mind-grenade')
+sayHi('susan');
+sayHi((names.john));
+sayHi(names.peter);
