56f0db740d9f1799d47b5b6e67994ba235eae99a
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-12-30 14:10:00 +0545

    Async method of reading files

---
+++ learning-nodejs/tutorial/6-fs-async.js
@@ -0,0 +1,18 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt','utf-8', (err, result) => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf-8', (err, result) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        const second = result;
+        writeFile('./content/result-async.txt',`Here is the async file ${first}, ${second}`)
+    })
+})
+    
